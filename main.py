676126d3062ea9a208a67d77ff327c346f3d33f3
jcsolano27 <104024333+jcsolano27@users.noreply.github.com>
2023-07-20 10:10:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ unzip_app(app_zip)
 
 
 print(os.listdir(f'Input/{path}/'))
-app_folder = os.listdir(f'Input/{path}/Microsoft.PowerApps/Apps')[0]  # 4568856658079073990
+app_folder = os.listdir(f'Input/{path}/Microsoft.PowerApps')[0]  # 4568856658079073990
 
 #msapp_path = getMsapp(path)
 #msapp_file = (msapp_path.split("/")[-1:])[0][:-6]
